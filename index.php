bb1c932cd7459be6edc85a9320e93ae215545a37
KrishnaGondaliya <31120886+KrishnaGondaliya@users.noreply.github.com>
2017-08-22 21:48:25 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,126 @@
+
+<html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+		<title>Psd to wordpress</title>
+		<link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
+	</head>
+	<body width=100%>
+		<div id="background">
+			<a href="#"><div id="footer"><img src="<?php echo get_template_directory_uri(); ?>/images/footer.png"></div></a>
+			<a href="#"><div id="Copyright2010StudioV"><img src="<?php echo get_template_directory_uri(); ?>/images/Copyright2010StudioV.png"></div></a>
+			<a href="#"><div id="SitePoweredbyWordpre"><img src="<?php echo get_template_directory_uri(); ?>/images/SitePoweredbyWordpre.png"></div></a>
+			<a href="#"><div id="Atveroeosetaccusamus"><img src="<?php echo get_template_directory_uri(); ?>/images/Atveroeosetaccusamus.png"></div></a>
+			<a href="#"><div id="Shape4"><img src="<?php echo get_template_directory_uri(); ?>/images/Shape4.png"></div></a>
+			<a href="#"><div id="Layer35"><img src="<?php echo get_template_directory_uri(); ?>/images/Layer35.png"></div></a>
+			<a href="#"><div id="Sedutperspiciatisund"><img src="<?php echo get_template_directory_uri(); ?>/images/Sedutperspiciatisund.png"></div></a>
+			<a href="#"><div id="Shape4copy"><img src="<?php echo get_template_directory_uri(); ?>/images/Shape4copy.png"></div></a>
+			<a href="#"><div id="Loremipsumdolorsitam"><img src="<?php echo get_template_directory_uri(); ?>/images/Loremipsumdolorsitam.png"></div></a>
+			<a href="#"><div id="Shape4copy3"><img src="<?php echo get_template_directory_uri(); ?>/images/Shape4copy3.png"></div></a>
+			<a href="#"><div id="Layer36"><img src="<?php echo get_template_directory_uri(); ?>/images/Layer36.png"></div></a>
+			<a href="#"><div id="Layer34"><img src="<?php echo get_template_directory_uri(); ?>/images/Layer34.png"></div></a>
+			<a href="#"><div id="NewsUpdates"><img src="<?php echo get_template_directory_uri(); ?>/images/NewsUpdates.png"></div></a>
+			<a href="#"><div id="layer_15"><img src="<?php echo get_template_directory_uri(); ?>/images/layer_15.png"></div></a>
+			<a href="#"><div id="layer_16"><img src="<?php echo get_template_directory_uri(); ?>/images/layer_16.png"></div></a>
+			<a href="#"><div id="layer_17"><img src="<?php echo get_template_directory_uri(); ?>/images/layer_17.png"></div></a>
+			<a href="#"><div id="layer_18"><img src="<?php echo get_template_directory_uri(); ?>/images/layer_18.png"></div></a>
+			<a href="#"><div id="Shape3copy9"><img src="<?php echo get_template_directory_uri(); ?>/images/Shape3copy9.png"></div></a>
+			<a href="#"><div id="Layer26copy2"><img src="<?php echo get_template_directory_uri(); ?>/images/Layer26copy2.png"></div></a>
+			<a href="#"><div id="VisitourBlog"><img src="<?php echo get_template_directory_uri(); ?>/images/VisitourBlog.png"></div></a>
+			<a href="#"><div id="copy7"><img src="<?php echo get_template_directory_uri(); ?>/images/copy7.png"></div></a>
+			<a href="#"><div id="Shape3copy"><img src="<?php echo get_template_directory_uri(); ?>/images/Shape3copy.png"></div></a>
+			<a href="#"><div id="yourname"><img src="<?php echo get_template_directory_uri(); ?>/images/yourname.png"></div></a>
+			<a href="#"><div id="Shape3copy6"><img src="<?php echo get_template_directory_uri(); ?>/images/Shape3copy6.png"></div></a>
+			<a href="#"><div id="youremailaddresscopy"><img src="<?php echo get_template_directory_uri(); ?>/images/youremailaddresscopy.png"></div></a>
+			<a href="#"><div id="Shape3copy7"><img src="<?php echo get_template_directory_uri(); ?>/images/Shape3copy7.png"></div></a>
+			<a href="#"><div id="message"><img src="<?php echo get_template_directory_uri(); ?>/images/message.png"></div></a>
+			<a href="#"><div id="Shape3copy8"><img src="<?php echo get_template_directory_uri(); ?>/images/Shape3copy8.png"></div></a>
+			<a href="#"><div id="Layer26copy"><img src="<?php echo get_template_directory_uri(); ?>/images/Layer26copy.png"></div></a>
+			<a href="#"><div id="Sendit"><img src="<?php echo get_template_directory_uri(); ?>/images/Sendit.png"></div></a>
+			<a href="#"><div id="copy6"><img src="<?php echo get_template_directory_uri(); ?>/images/copy6.png"></div></a>
+			<a href="#"><div id="Contact"><img src="<?php echo get_template_directory_uri(); ?>/images/Contact.png"></div></a>
+			<a href="#"><div id="layer_34"><img src="<?php echo get_template_directory_uri(); ?>/images/layer_34.png"></div></a>
+			<a href="#"><div id="Newsletter"><img src="<?php echo get_template_directory_uri(); ?>/images/Newsletter.png"></div></a>
+			<a href="#"><div id="Shape3"><img src="<?php echo get_template_directory_uri(); ?>/images/Shape3.png"></div></a>
+			<a href="#"><div id="youremailaddress"><img src="<?php echo get_template_directory_uri(); ?>/images/youremailaddress.png"></div></a>
+			<a href="#"><div id="Atveroeosetaccusamus_0"><img src="<?php echo get_template_directory_uri(); ?>/images/Atveroeosetaccusamus_0.png"></div></a>
+			<a href="#"><div id="layer_39"><img src="<?php echo get_template_directory_uri(); ?>/images/layer_39.png"></div></a>
+			<a href="#"><div id="Shape3copy5"><img src="<?php echo get_template_directory_uri(); ?>/images/Shape3copy5.png"></div></a>
+			<a href="#"><div id="Layer26"><img src="<?php echo get_template_directory_uri(); ?>/images/Layer26.png"></div></a>
+			<a href="#"><div id="Subscribe"><img src="<?php echo get_template_directory_uri(); ?>/images/Subscribe.png"></div></a>
+			<a href="#"><div id="copy5"><img src="<?php echo get_template_directory_uri(); ?>/images/copy5.png"></div></a>
+			<a href="#"><div id="SocialConnection"><img src="<?php echo get_template_directory_uri(); ?>/images/SocialConnection.png"></div></a>
+			<a href="#"><div id="Atveroeosetaccusamus_1"><img src="<?php echo get_template_directory_uri(); ?>/images/Atveroeosetaccusamus_1.png"></div></a>
+			<a href="#"><div id="layer_46"><img src="<?php echo get_template_directory_uri(); ?>/images/layer_46.png"></div></a>
+			<a href="#"><div id="Layer21"><img src="<?php echo get_template_directory_uri(); ?>/images/Layer21.png"></div></a>
+			<a href="#"><div id="Layer22"><img src="<?php echo get_template_directory_uri(); ?>/images/Layer22.png"></div></a>
+			<a href="#"><div id="Layer23"><img src="<?php echo get_template_directory_uri(); ?>/images/Layer23.png"></div></a>
+			<a href="#"><div id="Layer24"><img src="<?php echo get_template_directory_uri(); ?>/images/Layer24.png"></div></a>
+			<a href="#"><div id="Layer25"><img src="<?php echo get_template_directory_uri(); ?>/images/Layer25.png"></div></a>
+			<a href="#"><div id="break"><img src="<?php echo get_template_directory_uri(); ?>/images/break.png"></div></a>
+			<a href="#"><div id="Layer20"><img src="<?php echo get_template_directory_uri(); ?>/images/Layer20.png"></div></a>
+			<a href="#"><div id="Layer20copy"><img src="<?php echo get_template_directory_uri(); ?>/images/Layer20copy.png"></div></a>
+			<a href="#"><div id="Layer20copy2"><img src="<?php echo get_template_directory_uri(); ?>/images/Layer20copy2.png"></div></a>
+			<a href="#"><div id="Shape2"><img src="<?php echo get_template_directory_uri(); ?>/images/Shape2.png"></div></a>
+			<a href="#"><div id="Shape2copy"><img src="<?php echo get_template_directory_uri(); ?>/images/Shape2copy.png"></div></a>
+			<a href="#"><div id="Shape2copy2"><img src="<?php echo get_template_directory_uri(); ?>/images/Shape2copy2.png"></div></a>
+			<a href="#"><div id="Layer17"><img src="<?php echo get_template_directory_uri(); ?>/images/Layer17.png"></div></a>
+			<a href="#"><div id="Layer15"><img src="<?php echo get_template_directory_uri(); ?>/images/Layer15.png"></div></a>
+			<a href="#"><div id="Layer19"><img src="<?php echo get_template_directory_uri(); ?>/images/Layer19.png"></div></a>
+			<a href="#"><div id="Sedutperspiciatisund_0"><img src="<?php echo get_template_directory_uri(); ?>/images/Sedutperspiciatisund_0.png"></div></a>
+			<a href="#"><div id="Nemoenimipsamvolupta"><img src="<?php echo get_template_directory_uri(); ?>/images/Nemoenimipsamvolupta.png"></div></a>
+			<a href="#"><div id="Nequeporroquisquames"><img src="<?php echo get_template_directory_uri(); ?>/images/Nequeporroquisquames.png"></div></a>
+			<a href="#"><div id="PerfectLogic"><img src="<?php echo get_template_directory_uri(); ?>/images/PerfectLogic.png"></div></a>
+			<a href="#"><div id="Allyouwantyourwebsit"><img src="<?php echo get_template_directory_uri(); ?>/images/Allyouwantyourwebsit.png"></div></a>
+			<a href="#"><div id="CompleteSolution"><img src="<?php echo get_template_directory_uri(); ?>/images/CompleteSolution.png"></div></a>
+			<a href="#"><div id="Atoolanythingandever"><img src="<?php echo get_template_directory_uri(); ?>/images/Atoolanythingandever.png"></div></a>
+			<a href="#"><div id="UberCulture"><img src="<?php echo get_template_directory_uri(); ?>/images/UberCulture.png"></div></a>
+			<a href="#"><div id="FreshModernandreadyf"><img src="<?php echo get_template_directory_uri(); ?>/images/FreshModernandreadyf.png"></div></a>
+			<a href="#"><div id="Layer20copy6"><img src="<?php echo get_template_directory_uri(); ?>/images/Layer20copy6.png"></div></a>
+			<a href="#"><div id="Shape3copy2"><img src="<?php echo get_template_directory_uri(); ?>/images/Shape3copy2.png"></div></a>
+			<a href="#"><div id="LearnMore"><img src="<?php echo get_template_directory_uri(); ?>/images/LearnMore.png"></div></a>
+			<a href="#"><div id="copy2"><img src="<?php echo get_template_directory_uri(); ?>/images/copy2.png"></div></a>
+			<a href="#"><div id="Layer20copy5"><img src="<?php echo get_template_directory_uri(); ?>/images/Layer20copy5.png"></div></a>
+			<a href="#"><div id="Shape3copy3"><img src="<?php echo get_template_directory_uri(); ?>/images/Shape3copy3.png"></div></a>
+			<a href="#"><div id="LearnMore_0"><img src="<?php echo get_template_directory_uri(); ?>/images/LearnMore_0.png"></div></a>
+			<a href="#"><div id="copy3"><img src="<?php echo get_template_directory_uri(); ?>/images/copy3.png"></div></a>
+			<a href="#"><div id="Layer20copy4"><img src="<?php echo get_template_directory_uri(); ?>/images/Layer20copy4.png"></div></a>
+			<a href="#"><div id="Shape3copy4"><img src="<?php echo get_template_directory_uri(); ?>/images/Shape3copy4.png"></div></a>
+			<a href="#"><div id="LearnMore_1"><img src="<?php echo get_template_directory_uri(); ?>/images/LearnMore_1.png"></div></a>
+			<a href="#"><div id="copy4"><img src="<?php echo get_template_directory_uri(); ?>/images/copy4.png"></div></a>
+			<a href="#"><div id="break_0"><img src="<?php echo get_template_directory_uri(); ?>/images/break_0.png"></div></a>
+			<a href="#"><div id="shadow"><img src="<?php echo get_template_directory_uri(); ?>/images/shadow.png"></div></a>
+			<a href="#"><div id="Layer10"><img src="<?php echo get_template_directory_uri(); ?>/images/Layer10.png"></div></a>
+			<a href="#"><div id="glow"><img src="<?php echo get_template_directory_uri(); ?>/images/glow.png"></div></a>
+			<a href="#"><div id="Shape1"><img src="<?php echo get_template_directory_uri(); ?>/images/Shape1.png"></div></a>
+			<a href="#"><div id="Layer5"><img src="<?php echo get_template_directory_uri(); ?>/images/Layer5.png"></div></a>
+		  <a href="#"><div id="Loremipsumdolorsitam_0"><img src="<?php echo get_template_directory_uri(); ?>/images/Loremipsumdolorsitam_0.png"></div></a>
+			<a href="#"><div id="Layer27"><img src="<?php echo get_template_directory_uri(); ?>/images/Layer27.png"></div></a>
+			<a href="#"><div id="Layer27copy2"><img src="<?php echo get_template_directory_uri(); ?>/images/Layer27copy2.png"></div></a>
+			<a href="#"><div id="Layer27copy3"><img src="<?php echo get_template_directory_uri(); ?>/images/Layer27copy3.png"></div></a>
+			<a href="#"><div id="Layer27copy"><img src="<?php echo get_template_directory_uri(); ?>/images/Layer27copy.png"></div></a>
+			<a href="#"><div id="Layer27copy4"><img src="<?php echo get_template_directory_uri(); ?>/images/Layer27copy4.png"></div></a>
+			<a href="#"><div id="Layer28"><img src="<?php echo get_template_directory_uri(); ?>/images/Layer28.png"></div></a>
+			<a href="#"><div id="Layer28copy"><img src="<?php echo get_template_directory_uri(); ?>/images/Layer28copy.png"></div></a>
+			<a href="#"><div id="Layer28copy2"><img src="<?php echo get_template_directory_uri(); ?>/images/Layer28copy2.png"></div></a>
+			<a href="#"><div id="Layer28copy3"><img src="<?php echo get_template_directory_uri(); ?>/images/Layer28copy3.png"></div></a>
+			<a href="#"><div id="Layer28copy4"><img src="<?php echo get_template_directory_uri(); ?>/images/Layer28copy4.png"></div></a>
+			<a href="#"><div id="POLO360"><img src="<?php echo get_template_directory_uri(); ?>/images/POLO360.png"></div></a>
+			<a href="#"><div id="MYKINDOFBUSINESSBLOG"><img src="<?php echo get_template_directory_uri(); ?>/images/MYKINDOFBUSINESSBLOG.png"></div></a>
+			<a href="#"><div id="HOME"><img src="<?php echo get_template_directory_uri(); ?>/images/HOME.png"></div></a>
+			<a href="#"><div id="PRODUCTS"><img src="<?php echo get_template_directory_uri(); ?>/images/PRODUCTS.png"></div></a>
+			<a href="#"><div id="SERVICES"><img src="<?php echo get_template_directory_uri(); ?>/images/SERVICES.png"></div></a>
+			<a href="#"><div id="BLOG"><img src="<?php echo get_template_directory_uri(); ?>/images/BLOG.png"></div></a>
+			<a href="#"><div id="CONTACT"><img src="<?php echo get_template_directory_uri(); ?>/images/CONTACT.png"></div></a>
+		  <a href="#"><div id="Backtohome"><img src="<?php echo get_template_directory_uri(); ?>/images/Backtohome.png"></div></a>
+			<a href="#"><div id="Whatwehaveforyou"><img src="<?php echo get_template_directory_uri(); ?>/images/Whatwehaveforyou.png"></div></a>
+			<a href="#"><div id="Thingswedo"><img src="<?php echo get_template_directory_uri(); ?>/images/Thingswedo.png"></div></a>
+			<a href="#"><div id="Followourupdates"><img src="<?php echo get_template_directory_uri(); ?>/images/Followourupdates.png"></div></a>
+			<a href="#"><div id="Waystoreachus"><img src="<?php echo get_template_directory_uri(); ?>/images/Waystoreachus.png"></div></a>
+			<a href="#"><div id="copy4_0"><img src="<?php echo get_template_directory_uri(); ?>/images/copy4_0.png"></div></a>
+			<a href="#"><div id="Layer31"><img src="<?php echo get_template_directory_uri(); ?>/images/Layer31.png"></div></a>
+			<a href="#"><div id="Shape4copy2"><img src="<?php echo get_template_directory_uri(); ?>/images/Shape4copy2.png"></div></a>
+			<a href="#"><div id="Layer30"><img src="<?php echo get_template_directory_uri(); ?>/images/Layer30.png"></div></a>
+		</div>
+ </body>
+ </html>
